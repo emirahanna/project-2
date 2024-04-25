d0ceb673c971a060b41f8af6191a6e023ec8d90d
Emira Y <emirahanna@gmail.com>
2024-04-25 18:08:45 -0400

    trying to make the dialog work on vercel

--- src/dialog-box.js
+++ src/dialog-box.js
@@ -1,7 +1,7 @@
-import { html, css } from "lit";
-import { DDD } from "@lrnwebcomponents/d-d-d/d-d-d.js";
+import { LitElement, html, css } from "lit";
+import  "./media-image.js"
 
-export class DialogBox extends DDD {
+export class DialogBox extends LitElement {
   static get tag() {
     return "dialog-box";
   }
@@ -15,7 +15,14 @@ export class DialogBox extends DDD {
 
   static get styles() {
     return css`
+    super.styles,
       :host {
+        --container-width: 70vw;
+        --container-height: 100vh;
+        display: none;
+      }
+
+      :host([open]){
         display: block;
       }
 
@@ -30,16 +37,15 @@ export class DialogBox extends DDD {
       }
 
       .container {
-        height: 100%;
-        width: 70%;
-        display: flex;
-        margin: 20px;
-        padding: 20px;
+        height: var(--container-height);
+        width: var(--container-width);
+        margin: 24px;
+        padding: 24px;
         position: fixed;
-        transition: all 250ms ease-in;
-        border-radius: 8px;
+        border-radius: 16px;
+        display: block;
         background-color: white;
-        border: 1px solid black; /* Ensure it's above other content */
+        border: 4px solid black; /* Ensure it's above other content */
         z-index: 999;
         top: 50%;
         left: 50%;
@@ -49,10 +55,19 @@ export class DialogBox extends DDD {
         ); /* so the dialog box is centered (if this isnt here its like cut off by the screen) */
       }
 
+      .panel {
+        
+        transition: all 250ms ease-in;
+        display: flex;
+        justify-content: center;
+      }
+
       .pic {
-        width: 30vw;
-        height: 30vw;
+        width: 60vw;
+        height: 70vh;
         object-fit: contain;
+        border: 4px solid var(--media-image-primary-color-3);
+        border-radius: 8px;
       }
 
       .x-button {
@@ -64,33 +79,47 @@ export class DialogBox extends DDD {
         float: right;
       }
 
+      .next-button,
+      .prev-button {
+        font-size: 32px;
+        background-color: var(--media-image-primary-color-3);
+        width: 5vw;
+        height: 10vh;
+        border: none; /* Remove borders */
+      }
+
       .x-button:hover,
       .x-button:focus,
       .x-button:active {
         background-color: maroon;
       }
+
+      .next-button:hover,
+      .next-button:focus,
+      .next-button:active{
+        background-color: var(--media-image-primary-color-2);
+      }
     `;
   }
 
   render() {
-    console.log("rendered dialog box")
+    console.log("rendered dialog box");
     if (!this.open) {
       return html``;
     }
     return html` <div class="shadow">
-
-      <div>
+      <div class="container">
         <button class="x-button" @click=${this.openFalse}>X</button>
-        <div class="container">
+      
+          <div>${this.displaySlide()}</div>
+          <div class="panel">
           <button
             class="prev-button"
             @click=${this.prevSlide}
             ?disabled="${this.index === 0}"
           >
             ←
-          </button>
-          <div>${this.displaySlide()}</div>
-          <button
+          </button><button
             class="next-button"
             @click=${this.nextSlide}
             ?disabled="${this.index === this.slides.length - 1}"
@@ -107,15 +136,18 @@ export class DialogBox extends DDD {
     this.addOpenImageEventListener();
   }
 
+  
   addOpenImageEventListener() {
-    document.body.addEventListener("open-dialog", this.openTrue.bind(this));
+    window.addEventListener("open-dialog", this.openTrue.bind(this));
     console.log("added event listener");
   }
+  
+ 
 
   openTrue() {
     this.open = true;
     this.disableScroll();
-    console.log("open dialog")
+    console.log("open dialog");
   }
 
   openFalse() {
@@ -123,15 +155,12 @@ export class DialogBox extends DDD {
     this.enableScroll();
   }
 
-  displayIndex() {
-    return html`<p>Slide ${this.index + 1} / ${this.slides.length}</p>`;
-  }
 
   displaySlide() {
-    return html`<img class="pic" src="${this.slides[this.index].content}" />
-      <h1>${this.slides[this.index].caption}</h1>
-      <p>${this.slides[this.index].description}</p>
-      ${this.displayIndex()}`;
+    return html`<div><img class="pic" src="${this.slides[this.index].content}" />
+    <p style="right: 8px; position:absolute;z-index:1000;">Slide ${this.index + 1} / ${this.slides.length}</p>
+    <h1>${this.slides[this.index].caption}</h1>
+      <p>${this.slides[this.index].description}</p></div>`;
   }
 
   nextSlide() {
@@ -148,61 +177,45 @@ export class DialogBox extends DDD {
   }
 
   populateSlide() {
-    document.body
-      .querySelectorAll("media-image")
-      .forEach((image) => 
-      this.pushSlide(image));
+    //pushes image to the slides array and sets the index of the image based on where it is in the document
+    document.body.querySelectorAll("media-image").forEach((image) => {
+      image.index = this.slides.length;
+      this.slides.push(image);
+    });
   }
-
-  pushSlide(image){ //pushes image to the slides array and sets the index of the image based on where it is in the document
-    image.index = this.slides.length;
-    this.slides.push(image);
-  }
-  
 
   disableScroll() {
-   // get the current page scroll position (saves the pixels that have been scroller vertically and horizontally)
-   const scrollTop =  document.documentElement.scrollTop;
-   const scrollLeft =  document.documentElement.scrollLeft;
-   
-   // Store the current scroll position
-   this._scrollPosition = { top: scrollTop, left: scrollLeft };
+    // get the current page scroll position (saves the pixels that have been scroller vertically and horizontally)
+    const scrollTop = document.documentElement.scrollTop;
+    const scrollLeft = document.documentElement.scrollLeft;
 
-   // Disable scrolling
-   document.body.style.overflow = 'hidden';
+    // Store the current scroll position
+    this._scrollPosition = { top: scrollTop, left: scrollLeft };
+
+    // Disable scrolling
+    document.body.style.overflow = "hidden";
   }
 
   enableScroll() {
-        // Resetting to Enable scrolling
-        document.body.style.overflow = '';
+    // Resetting to Enable scrolling
+    document.body.style.overflow = "";
 
-        // Restore the scroll position
-        if (this._scrollPosition) {
-            window.scrollTo(this._scrollPosition.left, this._scrollPosition.top);
-        }
+    // Restore the scroll position
+    if (this._scrollPosition) {
+      window.scrollTo(this._scrollPosition.left, this._scrollPosition.top);
+    }
   }
 
   static get properties() {
     return {
-      title: { type: String},
-      text: { type: String},
+      ...super.properties,
+      title: { type: String },
+      text: { type: String },
       open: { type: Boolean, reflect: true },
-      slides: { type: Array},
+      slides: { type: Array },
     };
   }
 }
 
 globalThis.customElements.define(DialogBox.tag, DialogBox);
 
-// register globally so we can make sure there is only one
-window.DialogBox = window.DialogBox || {};
-// request if this exists. This helps invoke the element existing in the dom
-// as well as that there is only one of them. That way we can ensure everything
-// is rendered through the same modal
-window.DialogBox.requestAvailability = () => {
-  if (!window.DialogBox.instance) {
-    window.DialogBox.instance = document.createElement("dialog-box");
-    document.body.appendChild(window.DialogBox.instance);
-  }
-  return window.DialogBox.instance;
-};
--- src/media-image.js
+++ src/media-image.js
@@ -1,134 +1,131 @@
-import { html, css } from "lit";
-import { DialogBox } from "./dialog-box.js"; //Needed or else Media Image is never defined
-import { DDD } from "@lrnwebcomponents/d-d-d/d-d-d.js";
+  import { LitElement, html, css } from "lit";
+  /**
+   * Now it's your turn. Here's what we need to try and do:
+   * 1. Get you HTML from your card working in here
+   * 2. Get your CSS rescoped as needed to work here
+   * pls help i need to sleep but i also need to do this but i also need to sleep this is a cry for help
+   * b
+   */
 
-/**
- * Now it's your turn. Here's what we need to try and do:
- * 1. Get you HTML from your card working in here
- * 2. Get your CSS rescoped as needed to work here
- * pls help i need to sleep but i also need to do this but i also need to sleep this is a cry for help
- * b
- */
+  export class MediaImage extends LitElement {
+    static get tag() {
+      return "media-image";
+    }
 
-export class MediaImage extends DDD {
-  static get tag() {
-    return "media-image";
-  }
+    constructor() {
+      super();
+      this.index = 0;
+      this.content =
+        "https://preview.redd.it/02411nxds2m91.png?width=640&crop=smart&auto=webp&s=3a91ab902fdab402e4bcb84a3907bb0a2bad2d9b";
+      this.caption = "caption";
+      this.description = "description";
+    }
 
-  constructor() {
-    super();
-    this.index = 0;
-    this.content =
-      "https://preview.redd.it/02411nxds2m91.png?width=640&crop=smart&auto=webp&s=3a91ab902fdab402e4bcb84a3907bb0a2bad2d9b";
-    this.caption = "caption";
-    this.description = "description";
-  }
+    static get styles() {
+      return css`
+        :host {
+          --media-image-primary-color-1: #333;
+          --media-image-primary-color-2: navy;
+          --media-image-primary-color-3: yellow;
+          --media-image-secondary-color-1: white;
+          --media-image-secondary-color-2: lightgray;
+          --media-image-secondary-color-3: red;
+          color: black;
+          display: block;
+          padding: 20px;
+          margin-bottom: 20px;
+          text-align: center;
+        }
 
-  static get styles() {
-    return css`
-      :host {
-        --media-image-primary-color-1: #333;
-        --media-image-primary-color-2: navy;
-        --media-image-primary-color-3: yellow;
-        --media-image-secondary-color-1: white;
-        --media-image-secondary-color-2: lightgray;
-        --media-image-secondary-color-3: red;
-        color: black;
-        display: block;
-        padding: 20px;
-        margin-bottom: 20px;
-        text-align: center;
-      }
+        .image {
+          max-width: var(--media-image-width, 40vw);
+          height: var(--media-image-height, auto);
+          overflow: hidden;
+          object-fit: fil;
+          border-radius: 12px;
+          border: 4px solid var(--media-image-primary-color-1);
+          transition: all 0.3s ease-in;
+        }
+        .info-panel {
+          display: block;
+          text-align: left;
+        }
+        .button-panel {
+          display: inline-flex;
+          margin: auto;
+          padding: auto;
+        }
 
-      .image {
-        max-width: var(--media-image-width, 40vw);
-        height: var(--media-image-height, auto);
-        overflow: hidden;
-        object-fit: fil;
-        border-radius: 12px;
-        border: 4px solid var(--media-image-primary-color-1);
-        transition: all 0.3s ease-in;
-      }
-      .info-panel {
-        display: block;
-        text-align: left;
-      }
-      .button-panel {
-        display: inline-flex;
-        margin: auto;
-        padding: auto;
-      }
+        .image:hover,
+        .image:focus,
+        .image:active {
+          cursor: pointer;
+          transform: translate(8px, -8px);
+          box-shadow: grey -10px 10px;
+        }
+      `;
+    }
 
-      .image:hover,
-      .image:focus,
-      .image:active {
-        cursor: pointer;
-        transform: translate(8px, -8px);
-        box-shadow: black -10px 10px;
-      }
-    `;
-  }
-
-  render() {
-    return html`<div>
-      <div class="image-panel">
-        ${this.displayContent()}
-        <div class="info-panel">
-          <h1>${this.caption}</h1>
+    render() {
+      return html`<div>
+        <div class="image-panel">
+          ${this.displayContent()}
+          <div class="info-panel">
+            <h1>${this.caption}</h1>
+          </div>
         </div>
       </div>
-    </div>
-    `;
-  }
+      `;
+    }
 
 
-  displayContent() {
-    //making it so that the media type is determined by the file extension
-    if (
-      this.content.includes("jpg") ||
-      this.content.includes("jpeg") ||
-      this.content.includes("png") ||
-      this.content.includes("gif")
-    ) {
-      return html`<img class="image" src="${this.content}" @click=${this.contentClicked}/>`;
-    } else if (
-      this.content.includes("mp4") ||
-      this.content.includes("mov") ||
-      this.content.includes("avi")
-    ) {
-      return html`<video class="image" src="${this.content}" controls @click=${this.contentClicked}></video>`;
-    } else if (
-      this.content.includes("mp3") ||
-      this.content.includes("wav") ||
-      this.content.includes("flac")
-    ) {
-      return html`<audio class="image" src="${this.content}" controls @click=${this.contentClicked}></audio>`;
-    } else {
-      return html`<p>Invalid media type</p>`;
+    displayContent() {
+      //making it so that the media type is determined by the file extension
+      if (
+        this.content.includes("jpg") ||
+        this.content.includes("jpeg") ||
+        this.content.includes("png") ||
+        this.content.includes("gif")
+      ) {
+        return html`<img class="image" src="${this.content}" @click=${this.contentClicked}/>`;
+      } else if (
+        this.content.includes("mp4") ||
+        this.content.includes("mov") ||
+        this.content.includes("avi")
+      ) {
+        return html`<video class="image" src="${this.content}" controls @click=${this.contentClicked}></video>`;
+      } else if (
+        this.content.includes("mp3") ||
+        this.content.includes("wav") ||
+        this.content.includes("flac")
+      ) {
+        return html`<audio class="image" src="${this.content}" controls @click=${this.contentClicked}></audio>`;
+      } else {
+        return html`<p>Invalid media type</p>`;
+      }
+    }
+
+    contentClicked(){
+      this.createOpenGalleryEvent();
+      this.updateIndex();
+    }
+
+    updateIndex(){
+      document.body.querySelector("dialog-box").index = this.index;
+    }
+
+    createOpenGalleryEvent(){
+      this.dispatchEvent(new CustomEvent("open-dialog", {bubbles: true, composed: true}));
+    }
+
+    static get properties() {
+      return {
+        content: { type: String, Reflect: true },
+        caption: { type: String, Reflect: true },
+        description: { type: String, Reflect: true },
+        index: { type: Number, Reflect: true },
+      };
     }
   }
 
-  contentClicked(){
-    this.createOpenGalleryEvent();
-    this.updateIndex();
-  }
-
-  updateIndex(){
-    document.body.querySelector("dialog-box").index = this.index;
-  }
-
-  createOpenGalleryEvent(){
-    this.dispatchEvent(new CustomEvent("open-dialog", {bubbles: true, composed: true}));
-  }
-
-  static get properties() {
-    return {
-      content: { type: String, Reflect: true },
-      caption: { type: String, Reflect: true },
-      description: { type: String, Reflect: true },
-      index: { type: Number, Reflect: true },
-    };
-  }
-}
-
-globalThis.customElements.define(MediaImage.tag, MediaImage);
+  globalThis.customElements.define(MediaImage.tag, MediaImage);
